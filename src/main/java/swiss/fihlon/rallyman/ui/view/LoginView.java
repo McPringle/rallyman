fa71c44675613b1d549745ded99e6ed904aa2c16
Marcus Fihlon <marcus@fihlon.swiss>
2025-01-02 15:05:34 +0100

    💄 add basic user login form #14
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

---
+++ src/main/java/swiss/fihlon/rallyman/ui/view/LoginView.java
@@ -0,0 +1,41 @@
+/*
+ * RallyMan - Organizing rallies as easily as possible
+ * Copyright (C) Marcus Fihlon and the individual contributors to RallyMan.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package swiss.fihlon.rallyman.ui.view;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@Route("login")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout {
+
+    public LoginView() {
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        final var login = new LoginForm();
+        login.setAction("login");
+
+        add(new H1("RallyMan"), login);
+    }
+
+}
