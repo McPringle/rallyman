885a96c8c06e8114e7d2b36ad8eca1064cc8199f
Marcus Fihlon <marcus@fihlon.swiss>
2025-01-03 13:05:47 +0100

    🗃️ add test data for user login #14
    Signed-off-by: Marcus Fihlon <marcus@fihlon.swiss>

---
+++ src/test/java/swiss/fihlon/rallyman/data/TestUser.java
@@ -0,0 +1,25 @@
+/*
+ * RallyMan - Organizing rallies as easily as possible
+ * Copyright (C) Marcus Fihlon and the individual contributors to RallyMan.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package swiss.fihlon.rallyman.data;
+
+public interface TestUser {
+
+    String EMAIL = "test@localhost";
+    String PASSWORD = "testPassword12345";
+
+}
